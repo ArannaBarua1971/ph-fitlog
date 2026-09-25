bf24a4e7b62a9a242ee5ea1c79e5fc819ab885db
ArannaBarua1971 <arannabaruase71@gmail.com>
2026-09-25 12:02:49 +0600

    footer section added and add footer seciton in mainlayout

---
+++ src/app/components/layout/Footer.tsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <div className="flex  justify-between py-10 mt-5">
+            <div className="flex items-center gap-2">
+                <i className="fa-solid fa-dumbbell text-foreground"></i>
+                <span className="font-bold text-white text-[14px]">FITLOG</span>
+            </div>
+            <div className="content text-3 text-[#6B7280]">
+                <p>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
+            </div>
+        </div>
+  )
+}
+
+export default Footer
